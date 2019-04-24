cc31b9b2e91aa26daf0a39785473b6cbaa883bb8
szaizen <zaizen.saki@gmail.com>
2019-04-24 17:18:53 +0900

    first commit , Create outline

---
+++ js/codepen.js
@@ -0,0 +1,20 @@
+
+$('.js-game-1').on('click',function(){
+	console.log('test');
+  showModal('いつ？誰と？何する？！ゲーム！','<p class="codepen" data-theme-id="0" data-default-tab="js,result" data-user="szaizen" data-slug-hash="PgWbgE" style="box-sizing: border-box; display: flex; align-items: center; justify-content: center; border: 2px solid black; margin: 1em 0; padding: 1em;" data-pen-title="When?Who?What?GAME!!"><span>See the Pen <a href="https://codepen.io/szaizen/pen/PgWbgE/">When?Who?What?GAME!!</a> by szaizen (<a href="https://codepen.io/szaizen">@szaizen</a>)on <a href="https://codepen.io">CodePen</a>.</span></p><script async src="https://static.codepen.io/assets/embed/ei.js"></script>');
+});
+$('.js-game-2').on('click',function(){
+  showModal('おみくじ','<p class="codepen" data-theme-id="0" data-default-tab="css,result" data-user="szaizen" data-slug-hash="wZPXpR" style="box-sizing: border-box; display: flex; align-items: center; justify-content: center; border: 2px solid black; margin: 1em 0; padding: 1em;" data-pen-title="wZPXpR">  <span>See the Pen <a href="https://codepen.io/szaizen/pen/wZPXpR/">  wZPXpR</a> by szaizen (<a href="https://codepen.io/szaizen">@szaizen</a>)  on <a href="https://codepen.io">CodePen</a>.</span></p><script async src="https://static.codepen.io/assets/embed/ei.js"></script>');
+});
+$('.js-game-3').on('click',function(){
+  showModal('NumerOn','<p class="codepen" data-theme-id="0" data-default-tab="js,result" data-user="szaizen" data-slug-hash="gyXKKd" style="box-sizing: border-box; display: flex; align-items: center; justify-content: center; border: 2px solid black; margin: 1em 0; padding: 1em;" data-pen-title="NumerOn">  <span>See the Pen <a href="https://codepen.io/szaizen/pen/gyXKKd/">  NumerOn</a> by szaizen (<a href="https://codepen.io/szaizen">@szaizen</a>)  on <a href="https://codepen.io">CodePen</a>.</span></p><script async src="https://static.codepen.io/assets/embed/ei.js"></script>');
+});
+$('.js-study-1').on('click',function(){
+  showModal('ボタン無効化','<p class="codepen" data-theme-id="0" data-default-tab="css,result" data-user="szaizen" data-slug-hash="JVMzQB" style="box-sizing: border-box; display: flex; align-items: center; justify-content: center; border: 2px solid black; margin: 1em 0; padding: 1em;" data-pen-title="js課題1">  <span>See the Pen <a href="https://codepen.io/szaizen/pen/JVMzQB/">  js課題1</a> by szaizen (<a href="https://codepen.io/szaizen">@szaizen</a>)  on <a href="https://codepen.io">CodePen</a>.</span></p><script async src="https://static.codepen.io/assets/embed/ei.js"></script>');
+});
+$('.js-study-2').on('click',function(){
+  showModal('DOM操作','<p class="codepen" data-theme-id="0" data-default-tab="js,result" data-user="szaizen" data-slug-hash="ZZvZNm" style="box-sizing: border-box; display: flex; align-items: center; justify-content: center; border: 2px solid black; margin: 1em 0; padding: 1em;" data-pen-title="js課題2">  <span>See the Pen <a href="https://codepen.io/szaizen/pen/ZZvZNm/">  js課題2</a> by szaizen (<a href="https://codepen.io/szaizen">@szaizen</a>)  on <a href="https://codepen.io">CodePen</a>.</span></p><script async src="https://static.codepen.io/assets/embed/ei.js"></script>');
+});
+$('.js-study-3').on('click',function(){
+  showModal('Ajax、Cookie','<p class="codepen" data-theme-id="0" data-default-tab="js,result" data-user="szaizen" data-slug-hash="xepNMR" style="box-sizing: border-box; display: flex; align-items: center; justify-content: center; border: 2px solid black; margin: 1em 0; padding: 1em;" data-pen-title="js課題3">  <span>See the Pen <a href="https://codepen.io/szaizen/pen/xepNMR/">  js課題3</a> by szaizen (<a href="https://codepen.io/szaizen">@szaizen</a>)  on <a href="https://codepen.io">CodePen</a>.</span></p><script async src="https://static.codepen.io/assets/embed/ei.js"></script>');
+});
