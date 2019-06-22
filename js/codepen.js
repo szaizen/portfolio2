939d4135d9aba8c34f6d28db210e1f7f0fd8f425
szaizen <38065394+szaizen@users.noreply.github.com>
2019-06-22 18:28:42 +0900

    画像圧縮、モーダルレスポンシブ

--- js/codepen.js
+++ js/codepen.js
@@ -1,6 +1,5 @@
 
 $('.js-game-1').on('click',function(){
-	console.log('test');
   showModal('いつ？誰と？何する？！ゲーム！','<p class="codepen" data-theme-id="0" data-default-tab="js,result" data-user="szaizen" data-slug-hash="PgWbgE" style="box-sizing: border-box; display: flex; align-items: center; justify-content: center; border: 2px solid black; margin: 1em 0; padding: 1em;" data-pen-title="When?Who?What?GAME!!"><span>See the Pen <a href="https://codepen.io/szaizen/pen/PgWbgE/">When?Who?What?GAME!!</a> by szaizen (<a href="https://codepen.io/szaizen">@szaizen</a>)on <a href="https://codepen.io">CodePen</a>.</span></p><script async src="https://static.codepen.io/assets/embed/ei.js"></script>');
 });
 $('.js-game-2').on('click',function(){
@@ -20,28 +19,40 @@ $('.js-study-3').on('click',function(){
 });
 
 
-$('.work--card-1').on('click',function(){
- showModal('','<div class="work--list-modal">	<img src="image/enclass.png">	<div class="work--list-right">		<p class="work--list-title">			コミュニティ「Enclass」<br>			ブログ、イベント		</p>		<p class="work--list-detail">			プロジェクト人数：3名<br>			担当分野：フロントエンド開発<br>			使用ツール：Wordpress、Backlog、Slack<br>開発内容：一からWordpressのカスタムテーマを作成。		</p>	</div></div>');
+$('.work--card-5').on('click',function(){
+ showModal('','<div class="work--list-modal">   <div class="work--list-left">    <img src="image/display.png" class="image--display display--pc"><img src="image/display-sp.png" class="image--display display--sp">    <div class="image--pc">      <img src="image/enclass.png" class="image--scroll">    </div>  </div>  <div class="work--list-right">    <p class="work--list-title"> コミュニティ「Enclass」／ ブログ、イベント </p>    <p class="work--list-detail"> プロジェクト人数：3名<br> 担当分野：フロントエンド開発<br> 使用ツール：Wordpress、Backlog、Slack<br>開発内容：一からWordpressのカスタムテーマを作成。 </p>  </div></div>');
 });
 /*
-<div class="work--list-modal"> <img src="image/enclass.png">
+<div class="work--list-modal"> 
+  <div class="work--list-left">
+    <img src="image/display.png" class="image--display display--pc">
+    <img src="image/display-sp.png" class="image--display display--sp">
+    <div class="image--pc">
+      <img src="image/enclass.png" class="image--scroll">
+    </div>
+  </div>
   <div class="work--list-right">
-    <p class="work--list-title"> コミュニティ「Enclass」<br> ブログ、イベント </p>
+    <p class="work--list-title"> コミュニティ「Enclass」／ ブログ、イベント </p>
     <p class="work--list-detail"> プロジェクト人数：3名<br> 担当分野：フロントエンド開発<br> 使用ツール：Wordpress、Backlog、Slack<br>開発内容：一からWordpressのカスタムテーマを作成。 </p>
   </div>
 </div>
 */
 
-$('.work--card-2').on('click',function(){
- showModal('','<div class="work--list-modal">	<img src="image/pasonatechsystems.png">	<div class="work--list-right">		<p class="work--list-title">			「パソナテックシステムズ」<br>			コーポレートサイトリニューアル		</p>		<p class="work--list-detail">			URL：<a href="	https://systems.pasonatech.co.jp/" target="_blank">https://systems.pasonatech.co.jp/</a><br>			担当分野：メインデザイン、フロント開発<br>			使用ツール：Wordpress,asana,slack,AdobeXD<br>			開発内容：プロトタイプを作成し、デザインの考案。サイト内に使用する写真の選別。HTML,CSSやプラグインを使用してフロントのスタイル調整。<br>			開発期間：約3か月間<br>		</p>	</div></div>');
+$('.work--card-3').on('click',function(){
+ showModal('','<div class="work--list-modal">  <div class="work--list-left">    <img src="image/display.png" class="image--display display--pc"><img src="image/display-sp.png" class="image--display display--sp">    <div class="image--pc">      <img src="image/pasonatechsystems.png" class="image--scroll">    </div>  </div>  <div class="work--list-right">    <p class="work--list-title">      「パソナテックシステムズ」／コーポレートサイトリニューアル    </p>    <p class="work--list-detail">      URL：<a href=" https://systems.pasonatech.co.jp/" target="_blank">https://systems.pasonatech.co.jp/</a><br>      担当分野：メインデザイン、フロント開発<br>      使用ツール：Wordpress,asana,slack,AdobeXD<br>      開発内容：プロトタイプを作成し、デザインの考案。サイト内に使用する写真の選別。HTML,CSSやプラグインを使用してフロントのスタイル調整。<br>      開発期間：約3か月間<br>    </p>  </div></div>');
 });
 /*
 <div class="work--list-modal">
-	<img src="image/pasonatechsystems.png">
+  <div class="work--list-left">
+    <img src="image/display.png" class="image--display display--pc">
+<img src="image/display-sp.png" class="image--display display--sp">
+    <div class="image--pc">
+      <img src="image/pasonatechsystems.png" class="image--scroll">
+    </div>
+  </div>
 	<div class="work--list-right">
 		<p class="work--list-title">
-			「パソナテックシステムズ」<br>
-			コーポレートサイトリニューアル
+			「パソナテックシステムズ」／コーポレートサイトリニューアル
 		</p>
 		<p class="work--list-detail">
 			URL：<a href="	https://systems.pasonatech.co.jp/" target="_blank">https://systems.pasonatech.co.jp/</a><br>
@@ -55,40 +66,51 @@ $('.work--card-2').on('click',function(){
 
 */
 
-$('.work--card-3').on('click',function(){
- showModal('','<div class="work--list-modal">	<img src="image/music.png">	<div class="work--list-right">		<p class="work--list-title">			「M:usic」<br>			音楽販売風サイト（勉強用）		</p>		<p class="work--list-detail">			担当分野：全て<br>			使用ツール：Salesforce<br>			開発内容：Apex、Visualforceページを作成し、ログイン機能・ユーザ登録機能・カート機能を実装。<br>			開発期間：約1か月間<br>			Github：<a href="https://github.com/szaizen/music" target="_blank">https://github.com/szaizen/music</a><br>			Qiita：<a href="	https://qiita.com/szaizen/items/83f58c344c4478bb1520" target="_blank">https://qiita.com/szaizen/items/83f58c344c4478bb1520</a>		</p>	</div></div>');
+$('.work--card-4').on('click',function(){
+ showModal('','<div class="work--list-modal">  <div class="work--list-left">    <img src="image/display.png" class="image--display display--pc"><img src="image/display-sp.png" class="image--display display--sp">    <div class="image--pc">      <img src="image/music_pc.png" class="image--scroll">    </div>  </div>  <div class="work--list-right">    <p class="work--list-title">      「M:usic」／ 音楽販売風サイト（勉強用）    </p>    <p class="work--list-detail">      プロジェクト人数：1名<br>      使用ツール：Salesforce<br>      開発内容：Apex、Visualforceをフルスクラッチ開発し、ログイン機能・ユーザ登録機能・カート機能を実装。<br>       開発期間：約1か月間<br>      <a style="margin: 0 10px;" href="https://github.com/szaizen/music" target="_blank">Github</a><a style="margin: 0 10px;" href="https://qiita.com/szaizen/items/83f58c344c4478bb1520" target="_blank">Qiita</a>      </p>  </div></div>');
 });
 /* 
 <div class="work--list-modal">
-	<img src="image/music.png">
+  <div class="work--list-left">
+    <img src="image/display.png" class="image--display display--pc">
+<img src="image/display-sp.png" class="image--display display--sp">
+    <div class="image--pc">
+      <img src="image/music_pc.png" class="image--scroll">
+    </div>
+  </div>
   <div class="work--list-right">
     <p class="work--list-title">
-    	「M:usic」<br> 音楽販売風サイト（勉強用）
+    	「M:usic」／ 音楽販売風サイト（勉強用）
     </p>
     <p class="work--list-detail">
 			プロジェクト人数：1名<br>
     	使用ツール：Salesforce<br>
-    	開発内容：Apex、Visualforceページをフルスクラッチ開発し、ログイン機能・ユーザ登録機能・カート機能を実装。<br> 
+    	開発内容：Apex、Visualforceをフルスクラッチ開発し、ログイン機能・ユーザ登録機能・カート機能を実装。<br> 
     	開発期間：約1か月間<br>
-    	Github：<a href="https://github.com/szaizen/music" target="_blank">https://github.com/szaizen/music</a><br> 
-    	Qiita：<a href="	https://qiita.com/szaizen/items/83f58c344c4478bb1520" target="_blank">https://qiita.com/szaizen/items/83f58c344c4478bb1520</a> 
+    	<a style="margin: 0 10px;" href="https://github.com/szaizen/music" target="_blank">Github</a><a style="margin: 0 10px;" href="https://qiita.com/szaizen/items/83f58c344c4478bb1520" target="_blank">Qiita</a> 
     </p>
   </div>
 </div>
 */
 
-$('.work--card-4').on('click',function(){
- showModal('','<div class="work--list-modal">	<img src="image/signpost_pc.png">  <div class="work--list-right">    <p class="work--list-title">    	「Signpost」<br> 複業に関するメディア    </p>    <p class="work--list-detail">			URL：<a href="#" target="_blank">URLのっける</a><br>			開発人数：1名<br>    	使用ツール：Wordpress<br>    	開発内容：デザイン・コーディング・SEO対策・移行から運用、保守まで一人で担当<br>     	開発期間：約1ヶ月半<br>    </p>  </div></div>');
+$('.work--card-2').on('click',function(){
+ showModal('','<div class="work--list-modal">  <div class="work--list-left">    <img src="image/display.png" class="image--display display--pc"><img src="image/display-sp.png" class="image--display display--sp">    <div class="image--pc">      <img src="image/signpost.png" class="image--scroll">    </div>  </div>  <div class="work--list-right">    <p class="work--list-title">      「Signpost」／ 複業に関するメディア   </p>    <p class="work--list-detail">      URL：<a href="http://signpost.anotherworks.co.jp/" target="_blank">http://signpost.anotherworks.co.jp/</a><br>      開発人数：1名<br>      使用ツール：Wordpress<br>      開発内容：デザイン・コーディング・SEO対策・移行から運用、保守まで一人で担当<br>       開発期間：約1ヶ月半<br>    </p>  </div></div>');
 });
 /* 
 <div class="work--list-modal">
-	<img src="image/signpost_pc.png">
+  <div class="work--list-left">
+    <img src="image/display.png" class="image--display display--pc">
+<img src="image/display-sp.png" class="image--display display--sp">
+    <div class="image--pc">
+      <img src="image/signpost.png" class="image--scroll">
+    </div>
+  </div>
   <div class="work--list-right">
     <p class="work--list-title">
-    	「Signpost」<br> 複業に関するメディア
+    	「Signpost」／ 複業に関するメディア
     </p>
     <p class="work--list-detail">
-			URL：<a href="#" target="_blank">URLのっける</a><br>
+			URL：<a href="http://signpost.anotherworks.co.jp/" target="_blank">http://signpost.anotherworks.co.jp/</a><br>
 			開発人数：1名<br>
     	使用ツール：Wordpress<br>
     	開発内容：デザイン・コーディング・SEO対策・移行から運用、保守まで一人で担当<br> 
@@ -98,6 +120,27 @@ $('.work--card-4').on('click',function(){
 </div>
 */
 
-$('.work--card-5').on('click',function(){
- showModal('','');
+$('.work--card-1').on('click',function(){
+ showModal('','<div class="work--list-modal">  <div class="work--list-left">    <img src="image/display.png" class="image--display display--pc"><img src="image/display-sp.png" class="image--display display--sp">    <div class="image--pc">      <img src="image/anotherworks.png" class="image--scroll">    </div>  </div>  <div class="work--list-right">    <p class="work--list-title">      「Another works」<br> LP    </p>    <p class="work--list-detail">      URL：<a href="https://aw-anotherworks.com/" target="_blank">https://aw-anotherworks.com/</a><br>      開発内容：ランディングページを作成<br>       開発期間：約3日<br>    </p>  </div></div>');
+/*
+<div class="work--list-modal">
+  <div class="work--list-left">
+    <img src="image/display.png" class="image--display display--pc">
+<img src="image/display-sp.png" class="image--display display--sp">
+    <div class="image--pc">
+      <img src="image/anotherworks.png" class="image--scroll">
+    </div>
+  </div>
+  <div class="work--list-right">
+    <p class="work--list-title">
+      「Another works」<br> LP
+    </p>
+    <p class="work--list-detail">
+      URL：<a href="https://aw-anotherworks.com/" target="_blank">https://aw-anotherworks.com/</a><br>
+      開発内容：ランディングページを作成<br> 
+      開発期間：約3日<br>
+    </p>
+  </div>
+</div>
+*/
 });
